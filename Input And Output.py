7241b574683f715e2662604ea5aee427f09608a2
Akash Raut <136334052+Akash-Raut2000@users.noreply.github.com>
2024-12-11 21:53:35 +0530

    Add files via upload

---
+++ Input And Output.py
@@ -0,0 +1,9 @@
+# Using input() to get user input
+name = input("What is your name? ")
+age = input("How old are you? ")
+favorite_color = input("What is your favorite color? ")
+
+# Using print() to display output
+print("Hello,", name + "!")
+print("You are", age, "years old.")
+print("Your favorite color is", favorite_color + ".")
